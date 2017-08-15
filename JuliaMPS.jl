1ff42051bc5c9a10c1b7ba78869853c6d6bdf63f
Christopher White <christopher.white@alumni.rice.edu>
2017-08-15 10:47:25 -0700

    add utility functions for turning moments into something useful, exact Hamiltonians

--- JuliaMPS.jl
+++ JuliaMPS.jl
@@ -22,6 +22,9 @@ include("algebra.jl")
 
 export sanity_check, MPS, prod_σz_eigstate                #mps.jl
 export tittums, squeeze, ssqueeze, rfheis_frobenius2      #util.jl
+export chebyshev_polys, jackson_kernel, pauli_matrices_sparse #util.jl
+export chebyshev_polys, jackson_kernel, pauli_matrices_sparse #util.jl
+export rfheis_sparse #util.jl
 export chebyshev_traces, chebyshev_space, element         #algebra.jl
 export rfheis_W,  mpo, mpoeye, trace, canonical_form, ⊕   #mpo.jl
 export MPO, onsite_mpo, matrix_element                    #mpo.jl
--- util.jl
+++ util.jl
@@ -28,3 +28,56 @@ function rfheis_frobenius2(J :: Float64, h :: Array{Float64,1})
     return 2^L*(3*J*(L-1) + sum(h.^2))
 end
                     
+#this is kind of ridiculous
+#can do better either iteratively or (for certain x) by FFT method in Weiss et al.
+function chebyshev_polys(x :: Array{Float64,1}, n :: Int)
+    assert(all(abs(x .< 1)))
+    
+    N_pts = length(x)
+    
+    polys = zeros(N_pts, n)
+    
+    for m = 0:(n-1)
+        polys[:,m+1] = cos(m * acos(x))
+    end
+    return polys
+end
+
+function jackson_kernel(N :: Int)
+    m = 0:(N-1)
+    return ( (N - m + 1).*cos(π*m/(N+1)) + sin(π*m/(N+1))*cot(π/(N+1)) )/ (N+1)
+end
+
+
+#sparse Pauli matrices
+function pauli_matrices_sparse(L :: Int64)
+    sigx = sparse([0 1; 1 0])
+    sigy = sparse([0 -im; im 0])
+    sigz = sparse([1 0; 0 -1])
+    sigp = sparse([0 1; 0 0])
+    sigm = sparse([0 0; 1 0])
+    
+    X = [reduce(kron, (speye(2^(j-1)), sigx, speye(2^(L - j)))) for j in 1:L]
+    Y = [reduce(kron, (speye(2^(j-1)), sigy, speye(2^(L - j)))) for j in 1:L]
+    Z = [reduce(kron, (speye(2^(j-1)), sigz, speye(2^(L - j)))) for j in 1:L]
+    
+    P = [reduce(kron, (speye(2^(j-1)), sigp, speye(2^(L - j)))) for j in 1:L]
+    M = [reduce(kron, (speye(2^(j-1)), sigm, speye(2^(L - j)))) for j in 1:L]
+    
+    X = convert(Array{SparseMatrixCSC{Float64,Int64}, 1}, X)
+    Y = convert(Array{SparseMatrixCSC{Complex{Float64},Int64}, 1}, Y)
+    Z = convert(Array{SparseMatrixCSC{Float64,Int64}, 1}, Z)
+    P = convert(Array{SparseMatrixCSC{Float64,Int64}, 1}, P)
+    M = convert(Array{SparseMatrixCSC{Float64,Int64}, 1}, M)
+    return (X,Y,Z,P,M)
+end
+
+#sparse random field Heisenberg with specified fields
+function rfheis_sparse(J :: Number, hj :: Array{Float64, 1})
+    L = length(hj)
+    
+    (X,Y,Z,P,M) = pauli_matrices_sparse(L)
+    H = sum([X[l]*X[l+1] + Y[l]*Y[l+1] + Z[l]*Z[l+1] for l in 1:L-1])
+    H += sum([hj[l]*Z[l] for l in 1:L])
+    return H
+end
